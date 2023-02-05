2a7bbc546932d813b1d921baf0f7e21e684ac3e5
cherish <xieqin1991@gmail.com>
2023-02-05 21:35:55 +0800

    feat: update api list

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,26 +1,48 @@
 // import styles from "@/assets/styles/Home.module.css";
+import { Collapse, List } from "antd";
+
+import { IPath, IPathMethod } from "@/models";
 import { TagListSelector, useStore } from "@/stores";
 
-export default function HomePage() {
-  const tagList = useStore(TagListSelector);
-  console.log(tagList);
+const { Panel } = Collapse;
+
+export const ApiItem = ({ api }: { api: IPathMethod }) => {
+  return <List.Item>{api.summary}</List.Item>;
+};
+
+export const PathItem = ({ path }: { path: IPath }) => {
+  const item = (k: keyof typeof path) => path[k];
+  const list = Object.keys(path).map(k => item(k as keyof typeof path));
+  return <List dataSource={list} renderItem={api => <ApiItem api={api} />} />;
+};
+
+export const PathList = ({ paths }: { paths: IPath[] }) => {
   return (
-    <div>
-      {Object.keys(tagList).map((tag, i) => (
-        <div key={i}>
-          <div>{tag}</div>
-          <div>
-            {tagList[tag].map((item, _i) => (
-              <div key={_i}>
-                {Object.keys(item).map((method, _j) => {
-                  const _k = method as keyof typeof item;
-                  return <div key={_j}>{item[_k].summary}</div>;
-                })}
-              </div>
-            ))}
-          </div>
-        </div>
+    <>
+      {paths.map((item, i) => (
+        <PathItem key={i} path={item} />
       ))}
-    </div>
+    </>
   );
+};
+
+export const TagItem = ({ paths }: { paths: IPath[] }) => {
+  return <PathList paths={paths} />;
+};
+
+export const TagList = () => {
+  const tagList = useStore(TagListSelector);
+  return (
+    <Collapse ghost>
+      {Object.keys(tagList).map((tag, i) => (
+        <Panel key={i} header={tag}>
+          <TagItem paths={tagList[tag]} />
+        </Panel>
+      ))}
+    </Collapse>
+  );
+};
+
+export default function HomePage() {
+  return <TagList />;
 }
